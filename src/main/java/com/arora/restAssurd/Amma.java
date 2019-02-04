91fde02b36594be8c9270e148965470816d09299
ASHRAF <ASHRAF@DESKTOP-PHGH8V4>
2019-02-04 23:32:57 +0530

    Second Commit

--- src/main/java/com/arora/restAssurd/Amma.java
+++ src/main/java/com/arora/restAssurd/Amma.java
@@ -1,10 +1,23 @@
 package com.arora.restAssurd;
 
 public class Amma {
-
+	 public void Do() {
+		  
+		  
+		  
+	  }
+public void Do1() {
+		  
+		aaaa
+		  
+	  }
+public void Do2() {
+ 
+ 
+ 
+}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 
 	}
 
 }
